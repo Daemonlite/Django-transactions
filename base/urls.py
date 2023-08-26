dec88926f50813ca1c89ee813a6953bdc1b5b639
Eugene <khemikal2016@gmail.com>
2023-08-26 11:54:36 +0000

    asd

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from base.views import transfer_funds,register_user
+from base.views import transfer_funds,register_user,login_user,logout_user
 
 urlpatterns = [
     path('register/',register_user,name='register_user'),
+    path('login/',login_user,name='login'),
+    path('logout/',logout_user,name='logout'),
     path('transfer_funds/', transfer_funds,name='transfer_funds'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,78 +1,97 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpResponse
 from django.db import transaction
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from .models import Escrow_account, Profile
+from .models import Escrow_account, CustomUser
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from django.contrib.auth.hashers import make_password, check_password
 from .utils import generate_random_string
+from django.middleware import csrf
 
 
 @csrf_exempt
 def register_user(request):
-    if request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            age = data.get("age")
-            email = data.get("email")
-            passw = data.get("password")
-            full_name = data.get("full_name")
-            id_number = data.get("id_number")
-            address = data.get("address")
-            phone_number = data.get("phone_number")
-
-        except json.JSONDecodeError:
-            return JsonResponse(
-                {"message": "Invalid JSON data in the request body"}, status=400
-            )
-
-        password = make_password(passw)
-        # Check if all required fields are provided and not empty
-        if email and password and full_name and password:
-            try:
-                # Check if the provided email is valid
-                validate_email(email)
-            except ValidationError:
-                return JsonResponse({"message": "Invalid email format"}, status=400)
-            wallet_address = generate_random_string(30)
-            user = Profile.objects.create(
-                email=email,
-                password=password,
-                full_name=full_name,
-                age=age,
-                id_number=id_number,
-                address=address,
-                phone_number=phone_number,
-                btc_wallet_address=wallet_address,
-            )
-            response_data = {
-                "message": "User created successfully",
-                "user": {
-                    "id": user.id,
-                    "email": user.email,
-                    "full_name": user.full_name,
-                    "password": user.password,
-                    "age": user.age,
-                    "id_number": user.id_number,
-                    "address": user.address,
-                    "phone_number": user.phone_number,
-                    "btc_wallet_address": user.btc_wallet_address,
-                    "balance": user.balance
-                },
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        email = data['email']
+        password = data['password']
+        first_name = data['first_name']
+        last_name = data['last_name']
+        
+        user = CustomUser.objects.create_user(email=email, password=password, first_name=first_name, last_name=last_name)
+        
+        response_data = {
+            'message': 'User registered successfully',
+            'user': {
+                'id': user.id,
+                 'uid':user.identifier,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
             }
-            return JsonResponse(response_data)
-        else:
-            return JsonResponse({"message": "All fields are required"}, status=400)
+        }
+        return JsonResponse(response_data)
 
-    # Return a 405 Method Not Allowed response for other request methods
-    return HttpResponse(status=405)
+
+@csrf_exempt
+def login_user(request):
+   
+    if request.method != 'POST':
+        return HttpResponse(status=405)
+
+    try:
+        data = json.loads(request.body)
+        email = data.get('email')
+        password = data.get('password')
+    except json.JSONDecodeError:
+        return JsonResponse({'message': 'Invalid JSON data in the request body'}, status=400)
+
+    if not email or not password:
+        return JsonResponse({'message': 'Email and password are required'}, status=400)
+
+    try:
+        validate_email(email)
+    except ValidationError:
+        return JsonResponse({'message': 'Invalid email format'}, status=400)
+
+    try:
+        user = CustomUser.objects.get(email=email)
+    except CustomUser.DoesNotExist:
+        return JsonResponse({'message': 'User with the provided email does not exist'}, status=401)
+
+    if user.check_password(password):
+        login(request, user)
+        
+        csrf_token = csrf.get_token(request)
+        response_data = {
+            'message': 'Logged in successfully',
+            'user': {
+                'id': user.id,
+                'uid':user.identifier,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'password':user.password,
+                'csrf_token':csrf_token
+            }
+        }
+        return JsonResponse(response_data)
+    else:
+        return JsonResponse({'message': 'Invalid credentials'}, status=401)
+
+def logout_user(request):
+    logout(request)
+    return JsonResponse({'message': 'Logged out successfully'})
+
+
+
 
 
 @csrf_exempt
@@ -86,10 +105,10 @@ def transfer_funds(request):
 
         with transaction.atomic():
             from_wallet_address = get_object_or_404(
-                Profile, btc_wallet_address=from_wallet_address
+                CustomUser, btc_wallet_address=from_wallet_address
             )
             to_wallet_address = get_object_or_404(
-                Profile, btc_wallet_address=to_wallet_address
+                CustomUser, btc_wallet_address=to_wallet_address
             )
 
             if from_wallet_address.balance >= amount:
@@ -119,7 +138,7 @@ def deposit_escrow(request):
             escrow.usdt_balance += amount
             escrow.save()
 
-            user = get_object_or_404(Profile, id=data["user_id"])
+            user = get_object_or_404(CustomUser, id=data["user_id"])
             user.account_balance -= amount
             user.save()
 
